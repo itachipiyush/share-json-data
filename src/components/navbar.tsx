abb568486331592d1d28e0f84eee6121300adf78
Piyush <piyushjha.2312@gmail.com>
2024-10-13 14:57:02 +0530

    install prisma

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,7 @@
 import { ExternalLinkIcon } from "lucide-react";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Navbar() {
   return (
@@ -8,16 +9,26 @@ export default function Navbar() {
       <div className="flex items-center justify-between mx-auto max-w-4xl h-16">
         <div className="flex gap-4">
           <Link href="/" className="flex items-center gap-2">
-          <ExternalLinkIcon className="h-6 w-6"/>
-          <span className="font-bold">sharejson.</span>
+            <ExternalLinkIcon className="h-6 w-6" />
+            <span className="font-bold">sharejson.</span>
           </Link>
           <nav>
-            <Link href='/dashboard' className="text-sm font-medium text-muted-foreground transition-colors hover:text-foreground">
-             Dashboard
+            <Link
+              href="/dashboard"
+              className="text-sm font-medium text-muted-foreground transition-colors hover:text-foreground"
+            >
+              Dashboard
             </Link>
           </nav>
         </div>
-        <Button variant='outline'>Sign In</Button>
+        <SignedOut>
+          <SignInButton mode="modal">
+            <Button variant="outline">Sign In</Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
     </div>
   );
